e33b5f8d8a40598340f6eb899fd8ae2023779c22
karlajajic <33891069+karlajajic@users.noreply.github.com>
2020-05-12 15:55:43 +0200

    reloading the rest of the design
    currentdata, device and main-Karla changed, no files to be loaded :cocktail:

--- currentCondition.h
+++ currentCondition.h
@@ -3,7 +3,6 @@
 
 typedef struct currentCondition* currentCondition_t;
 
-<<<<<<< HEAD
 currentCondition_t currentCondition_create(int deviceId);
 
 void currentCondition_setCO2(currentCondition_t self, uint16_t value);
@@ -12,8 +11,3 @@ void currentCondition_setTemperature(currentCondition_t self, uint16_t value);
 void currentCondition_setSound(currentCondition_t self, uint16_t value);
 
 void currentCondition_destroy(currentCondition_t self);
-=======
-currentCondition_t currentCondition_create(int deviceId, uint16_t co2Data, uint16_t temperatureData, uint16_t humidityData, uint16_t soundData);
-void currentCondition_destroy(currentCondition_t self);
-//toString????
->>>>>>> parent of 32d18b3... changed design
